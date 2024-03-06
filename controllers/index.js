3f9ef9e82c978ff42019298766846c9c7c2bfde4
Mike Sheliga <msheliga1@gmail.com>
2024-03-05 20:13:28 -0500

    Put /, /h// - / get returns all users and associated weddings. and /login routes in controllers/homeRoutes.js, /hello in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 
-console.log("Starting to load controllers/index.js routes.");
 console.info("Starting to load controllers/index.js routes.");
 // const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes');  // Uncommented 3.5.24 ... now the fun begins.
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
 
-router.get('/', async (req, res) => {
+// MJS 3.4.24 - First simple route to get one working before loading several files for 
+// /api and homeroutes.
+router.get('/hello', async (req, res) => {
     try {
-        res.status(200).json("MJS home route found!");
+        res.status(200).json("MJS /hello route in controller/index.js found!");
     } catch (err) {
       res.status(500).json(err);
     }
