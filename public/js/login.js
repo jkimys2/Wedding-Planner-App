731d4a8ae4dc03857a290f4c509b13895fd84e21
Mike Sheliga <msheliga1@gmail.com>
2024-03-07 15:05:01 -0500

    Added util auth/auth2, public login/logout/profile.js files. All of these should have been bulk imported from a prior repo. Only profile.js needs changed.

---
+++ public/js/login.js
@@ -0,0 +1,59 @@
+// MJS 3.5.24 - Copied from uri act 14-28 mp.  Should not need any changes.
+const loginFormHandler = async (event) => {
+  console.log("login Form public/js Handler beginning ...");
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    console.log("login Form public/js Handler found email and password ...");
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log("Login Form handler got post response ...", response.status);
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      console.log("login form handler got response.ok ... going to profile page");
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("Sign-Up Form Handler beginning ...");
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// MJS 3.4.24 - Orig from my hw14 - uri act 14-28 mp
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
