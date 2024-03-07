0e40e74f4c3dcd48817631921edfab41df425c18
Mike Sheliga <msheliga1@gmail.com>
2024-03-07 18:57:02 -0500

    Profile - shows data and deletes, add not working yet.  hbs files: profile2, profile.orig, profile.html, js files profile and profile2

--- public/js/profile2.js
+++ public/js/profile2.js
@@ -5,28 +5,30 @@ const newFormHandler = async (event) => {
   const first_name = document.querySelector('#invitee-name').value.trim();
   const last_name = document.querySelector('#invitee-last').value.trim();
   const email = document.querySelector('#invitee-email').value.trim();
+  const plus_one = true; 
+  const accepted = false;
   const food_choice = "Steak";  // should allow null => unknown
 
   if (first_name && last_name && email) {
     console.log("Creating user ", first_name, last_name, email, food_choice); 
     const response = await fetch(`/api/invitees`, {
       method: 'POST',
-      body: JSON.stringify({first_name, last_name, email, food_choice }),
+      body: JSON.stringify({first_name, last_name, email, plus_one, accepted, food_choice }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/profile2');
     } else {
-      alert('Failed to create invitee');
+      alert('Failed to create invitee2');
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-    console.log("Profile delButtonHandler beginning ... ");
+    console.log("Profile2 delButtonHandler beginning ... ");
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
         console.log("Profile2 delButtonHandler found data-id of ", id);
