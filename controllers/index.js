d93f15b22aa7e61349b866686d7f589f2560a0d3
Mike Sheliga <msheliga1@gmail.com>
2024-03-07 19:08:06 -0500

    Key update allowing api routes to be accessed. This had to be off until all accessory/help/format files were loaded.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 console.info("Starting to load controllers/index.js routes.");
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');  // Uncommented 3.5.24 ... now the fun begins.
  
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // MJS 3.4.24 - First simple route to get one working before loading several files for 
 // /api and homeroutes.
